b32813c449fce163b6e5101380f6a106dc83a10e
Carla Gil <carlabgo95@gmail.com>
2022-09-12 09:36:50 -0300

    model pedido proveedores

---
+++ Back/Kiltex.SistemaGestion.Domain/Model/SupplierOrder.cs
@@ -0,0 +1,34 @@
+﻿using Kiltex.SistemaGestion.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiltex.SistemaGestion.Domain.Model
+{
+    [Table("supplier_order")]
+    public class SupplierOrder : BaseModel
+    {
+        [Required]
+        [Column("supplier_id")]
+        public long SupplierId { get; set; }
+
+        [ForeignKey("SupplierId")]
+        public Supplier Supplier { get; set; }
+
+        [Column("is_paid")]
+        public bool IsPaid { get; set; }
+
+        [Required]
+        [Column("status_id")]
+        public long StatusId { get; set; }
+
+        [NotMapped]
+        public ESupplierOrderDetailsStatuses Status { get => (ESupplierOrderDetailsStatuses)StatusId; }
+
+
+    }
+}
---
+++ Back/Kiltex.SistemaGestion.Domain/Model/SupplierOrderDetail.cs
@@ -0,0 +1,41 @@
+﻿using Kiltex.SistemaGestion.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiltex.SistemaGestion.Domain.Model
+{
+    [Table("supplier_order_detail")]
+    public class SupplierOrderDetail : BaseModel
+    {
+        [Required]
+        [Column("product_id")]
+        public long ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        [Column("supplier_order_id")]
+        public long SupplierOrderId { get; set; }
+
+        [ForeignKey("SupplierOrderId")]
+        public SupplierOrder SupplierOrder { get; set; }
+
+        [Required]
+        [Column("ordered_quantity")]
+        public int OrderedQuantity { get; set; }
+        [Required]
+        [Column("recieved_quantity")]
+        public int RecievedQuantity { get; set; }
+        [Required]
+        [Column("status_id")]
+        public long StatusId { get; set; }
+
+        [NotMapped]
+        public ESupplierOrderStatuses Status { get => (ESupplierOrderStatuses)StatusId; }
+    }
+}
