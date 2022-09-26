96e5153be610cd009c32724c6a51212f0c519293
Carla Gil <carlabgo95@gmail.com>
2022-09-26 13:14:34 -0300

    agrego servicios, controladores de pedidos a proveedores

---
+++ Back/Kiltex.SistemaGestion.Services/Models/Dtos/DtoSupplierOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiltex.SistemaGestion.Services.Models.Dtos
+{
+    public class DtoSupplierOrder
+    {
+        public long Id { get; set; }
+        public long SupplierId { get; set; }
+
+        public string SupplierName { get; set; }
+        public bool IsPaid { get; set; }
+
+        public long StatusId { get; set; }
+        public List<DtoOrderDetail> OrderDetail { get; set; }
+        
+
+    }
+    public class DtoOrderDetail
+    {
+        public long Id { get; set; }
+        public long ProductId { get; set; }
+
+        public int OrderedQuantity { get; set; }
+
+        public int RecievedQuantity { get; set; }
+        public long StatusId { get; set; }
+
+
+    }
+}
---
+++ Back/Kiltex.SistemaGestion.Services/Services/SupplierOrderService.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using Kiltex.SistemaGestion.Domain;
+using Kiltex.SistemaGestion.Domain.Model;
+using Kiltex.SistemaGestion.SDK.Error;
+using Kiltex.SistemaGestion.Services.Common;
+using Kiltex.SistemaGestion.Services.Models.Dtos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiltex.SistemaGestion.Services.Services
+{
+    public class SupplierOrderService : BaseService
+    {
+        public SupplierOrderService(ErrorManager logger, DBContext context, IMapper maper) :
+            base(logger, context, maper)
+        { }
+        public async Task<OperationResponse<DtoSupplierOrder>> GetById(long id)
+        {
+            SupplierOrder order = await _contextSql
+                                .SupplierOrders
+                                .Include(p => p.Supplier)
+                                .Include(p => p.SupplierOrderDetail)
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync(p => p.Id == id)
+                                .ConfigureAwait(false);
+            if (order == null)
+
+                return new OperationResponse<DtoSupplierOrder>(null, false, new OperationExceptions("000", $"Orden no encontrada {id}"));
+
+
+            DtoSupplierOrder result = _mapper.Map<DtoSupplierOrder>(order);
+
+            return new OperationResponse<DtoSupplierOrder>(result);
+        }
+        public async Task<OperationResponse<IdResponse<long>>> AddOrUpdate(DtoAddSupplierOrder model, CancellationToken ct = default)
+        {
+
+            SupplierOrder newOrder = _mapper.Map<SupplierOrder>(model);
+            if (newOrder.Id == 0)
+            {
+                newOrder.StatusId = 1;
+                await _contextSql.SupplierOrders.AddAsync(newOrder, ct).ConfigureAwait(false);
+
+            }
+            else
+            {
+                //var oldOrder = await _contextSql
+                //    .SupplierOrders
+                //    .AsNoTracking()
+                //    .Include(p => p.Supplier)
+                //    .Include(p => p.SupplierOrderDetail)
+                //    .FirstAsync(p => p.Id == newOrder.Id)
+                //    .ConfigureAwait(false);
+
+                _contextSql.SupplierOrders.Update(newOrder);
+
+
+            }
+            await _contextSql.SaveChangesAsync(ct).ConfigureAwait(false);
+
+            return Ok(new IdResponse<long>(newOrder.Id));
+        }
+        public async Task<OperationResponse<DtoPagination<DtoSupplierOrder>>> List(RequestPaginatedData<string> request, int? status)
+        {
+            var query = _contextSql
+                                .SupplierOrders
+                                .AsNoTracking()
+                                .Include(p => p.SupplierOrderDetail)
+                                .Include(p => p.Supplier)
+                                .Where(p => p.Supplier.Name.ToLower().Contains(request.Filter ?? "") && p.StatusId == status);
+
+            var count = await query.CountAsync().ConfigureAwait(false);
+
+            var list = await query.OrderBy(p => p.Id)
+                                  .Skip(request.Page * request.PageSize)
+                                  .Take(request.PageSize)
+                                  .ToListAsync()
+                                  .ConfigureAwait(false);
+
+            var dto = _mapper.Map<List<DtoSupplierOrder>>(list);
+
+
+            return new OperationResponse<DtoPagination<DtoSupplierOrder>>(new DtoPagination<DtoSupplierOrder>
+            {
+                Data = dto,
+                PageSize = request.PageSize,
+                TotalCount = count
+            });
+        }
+
+    }
+
+}
