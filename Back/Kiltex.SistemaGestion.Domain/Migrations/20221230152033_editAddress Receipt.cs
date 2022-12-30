969f9e8d0a27a1b8631b302cdb0a30f88f8ccd9b
Alexandro Luna <lunalebizkit@gmail.com>
2022-12-30 12:54:35 -0300

    subtotal y update product

---
+++ Back/Kiltex.SistemaGestion.Domain/Migrations/20221230152033_editAddress Receipt.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kiltex.SistemaGestion.Domain.Migrations
+{
+    public partial class editAddressReceipt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "supplier_adress",
+                table: "receipt",
+                newName: "supplier_address");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "supplier_address",
+                table: "receipt",
+                newName: "supplier_adress");
+        }
+    }
+}
