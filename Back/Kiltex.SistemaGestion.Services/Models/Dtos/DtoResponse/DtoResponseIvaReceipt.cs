e8565b9bc88734fa5dad3b08b05633233119ad8a
Carla Gil <carlabgo95@gmail.com>
2023-01-03 15:18:46 -0300

    arreglo metodo periodo compra

---
+++ Back/Kiltex.SistemaGestion.Services/Models/Dtos/DtoResponse/DtoResponseIvaReceipt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiltex.SistemaGestion.Services.Models.Dtos.DtoResponse
+{
+    public class DtoResponseIvaReceipt
+    {
+
+        public decimal PeriodTotal { get; set; }
+        public List<DtoResponseIvaReceipts> DtoResponseIvaReceipts { get; set; }
+
+
+    }
+    public class DtoResponseIvaReceipts
+    {
+        public long Id { get; set; }
+        public long InvoiceNumber { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierCuit { get; set; }
+        public string SupplierAddress { get; set; }
+        public decimal Total { get; set; }
+        public DateTime DateTime { get; set; }
+        public int Type { get; set; }
+        public decimal Iva21 { get; set; }
+        public decimal Iva27 { get; set; }
+        public decimal Iva10 { get; set; }
+        public decimal ImporteNetoIva21 { get; set; }
+        public decimal ImporteNetoIva27 { get; set; }
+        public decimal ImporteNetoIva10 { get; set; }
+        public decimal ImporteNeto { get; set; }
+
+        public decimal IvaTotal { get; set; }
+    }
+}
+
