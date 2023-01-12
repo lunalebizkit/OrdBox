808ff3904ce3f18c626e5c53dc86d31887e73013
Alexandro Luna <lunalebizkit@gmail.com>
2023-01-12 11:27:40 -0300

    agrego finally en ivaService

--- Back/Kiltex.SitemaGestion.Api/Controllers/Iva/IvaController.cs
+++ Back/Kiltex.SitemaGestion.Api/Controllers/Iva/IvaController.cs
@@ -31,7 +31,7 @@ namespace Kiltex.SistemaGestion.Api.Controllers.Iva
         }
         [HttpGet]
         [Route("ReceiptIvaReport")]
-        public async Task<IActionResult> ReceiptIvaReport(DateTime from, DateTime to)
+        public async Task<IActionResult> ReceiptIvaReport([FromQuery] DateTime from, DateTime to)
         {
             var content = await _service.ReceiptIvaReport(from, to ).ConfigureAwait(false);
             return File(content.Data, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"ListaReporteIvaCompra_{DateTime.Now:dd-MM-yyyy}.xlsx");
