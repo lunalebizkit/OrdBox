7a6a4aa85b2ffd17e06eb08fb82c150f6609afe8
Alexandro Luna <lunalebizkit@gmail.com>
2024-01-12 10:03:12 -0300

    create models for Invoce Reports

---
+++ Back/Kiltex.SistemaGestion.Services/Models/Dtos/DtoResponse/DtoResponseInviocesReport.cs
@@ -0,0 +1,12 @@
+﻿namespace Kiltex.SistemaGestion.Services.Models.Dtos.DtoResponse
+{
+    public class DtoResponseInviocesReport
+    {
+        public DateTimeOffset Date {  get; set; }
+        public string ProductName { get; set; }
+        public string CustomerName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
