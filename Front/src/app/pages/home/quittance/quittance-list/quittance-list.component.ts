992394c10591592559c024640ea172a4dccc0733
Alexandro Luna <lunalebizkit@gmail.com>
2024-01-19 12:40:29 -0300

    delete console log from quittance

--- Front/src/app/pages/home/quittance/quittance-list/quittance-list.component.ts
+++ Front/src/app/pages/home/quittance/quittance-list/quittance-list.component.ts
@@ -90,7 +90,6 @@ export class QuittanceListComponent implements OnInit {
         this.selectedIndex = 0;
         this.selectedQuittance = this.quittanceList[this.selectedIndex];
         document.getElementById(this.selectedIndex.toString())?.focus();
-        console.log(this.quittanceList);
       },
       error: () => {
         this.loading = false;
