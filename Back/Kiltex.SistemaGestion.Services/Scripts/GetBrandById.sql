32fd5abd22168e94779f14977cacbbd58b051c02
Alexandro Luna <lunalebizkit@gmail.com>
2024-02-02 15:49:33 -0300

    create script to get information from product, brand and category

---
+++ Back/Kiltex.SistemaGestion.Services/Scripts/GetBrandById.sql
@@ -0,0 +1,5 @@
+﻿  SELECT TOP(1) B.id, B.description FROM [brand] B
+  INNER JOIN [product] P
+  ON P.brand_id = B.id
+  WHERE B.id = @brandid
+  AND P.[is_deleted] = 0
---
+++ Back/Kiltex.SistemaGestion.Services/Scripts/GetProductById.sql
@@ -0,0 +1,5 @@
+﻿ SELECT TOP(1) * FROM [product] P
+  INNER JOIN [brand] B
+  ON P.brand_id = B.id
+  WHERE P.id = @productid
+  AND P.[is_deleted] = 0
