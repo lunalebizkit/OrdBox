65aa57e1233be8545854494dd551712a91f0377c
Ale Luna <lunalebizkit@gmail.com>
2025-05-26 10:21:16 -0300

    update version

--- Front/src/app/common/auth/auth.module.ts
+++ Front/src/app/common/auth/auth.module.ts
@@ -1,11 +1,6 @@
-import { NgModule } from '@angular/core';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NgModule } from '@angular/core';;
 import { AuthGuard } from './permission/auth.guard';
 import { PermissionService } from './permission/permission-manager.service';
-import { AuthModalComponent } from './auth-modal/auth-modal.component';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
   declarations: [],
