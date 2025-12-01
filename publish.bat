695cd8b3db43138728066fe5b50751bbc9760c56
Ale Luna <lunalebizkit@gmail.com>
2025-12-01 10:49:48 -0300

    set local docker image to save

--- publish.bat
+++ publish.bat
@@ -2,14 +2,14 @@
  cd Back
  
  IF NOT "%2" == "front" (
- docker build -f Dockerfile --tag fdufour/gestion:api-%1 --tag fdufour/gestion:api-latest .
- docker push fdufour/gestion:api-%1
- docker push fdufour/gestion:api-latest
+ docker build -f Dockerfile --tag lunale/gestion:api-%1 --tag lunale/gestion:api-latest .
+ docker push lunale/gestion:api-%1
+ docker push lunale/gestion:api-latest
  )
 
  cd ..\Front
  IF NOT "%2" == "api" (
- docker build -f Dockerfile --tag fdufour/gestion:front-%1 --tag fdufour/gestion:front-latest .
- docker push fdufour/gestion:front-%1
- docker push fdufour/gestion:front-latest
+ docker build -f Dockerfile --tag lunale/gestion:front-%1 --tag lunale/gestion:front-latest .
+ docker push lunale/gestion:front-%1
+ docker push lunale/gestion:front-latest
  )
