14ff6c88037433445a89f94d5c890c064d195cea
Ale Luna <lunalebizkit@gmail.com>
2026-05-11 09:52:40 -0300

    add Arca integracion interface and service classes

---
+++ Back/Kiltex.SistemaGestion.Services/ARCA/ArcaIntegracionService.cs
@@ -0,0 +1,6 @@
+﻿namespace Kiltex.SistemaGestion.Services.ARCA
+{
+    public class ArcaIntegracionService : IArcaIntegracion
+    {
+    }
+}
---
+++ Back/Kiltex.SistemaGestion.Services/ARCA/IArcaIntegracion.cs
@@ -0,0 +1,6 @@
+﻿namespace Kiltex.SistemaGestion.Services.ARCA
+{
+    public interface IArcaIntegracion
+    {
+    }
+}
